5055233b12122d3fdb8549af4a92ad3b516937fe
Clayton Schneider <claykschneider@gmail.com>
2016-01-14 17:01:59 -0800

    Scaffold initial routes and controller for users

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ source 'https://rubygems.org'
 gem 'rails', '4.2.5'
 # Use Postgres as the database for Active Record
 gem 'pg'
+# Use responder gem for API
+gem 'responders', '~>2.0'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -13,6 +13,15 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+
+  # Get repos
+  resources :repos, only: [:index] do
+  end
+
+  resources :users, only: [:index] do
+    resources :following, only: [:index, :update, :create, :show] do
+    end
+  end
 
   # Example resource route with options:
   #   resources :products do
